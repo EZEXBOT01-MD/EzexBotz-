d4913705ae0ce9da2eef5f01a47a4962816caf1e
EZEXBOT01-MD <108160500+EZEXBOT01-MD@users.noreply.github.com>
2022-06-26 22:48:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
+   * Create By Ezex station.
+   * Contact Me on wa.me/6283845930691
    * Follow https://github.com/DikaArdnt
 */
 
